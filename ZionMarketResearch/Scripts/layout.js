5ef9200f04eae26a57b79a638fadc5bb288b968f
prashantkudale <prashant.kudale1989@gmail.com>
2026-03-14 11:45:32 +0530

    Zion performance :: V1 :: Css and layout changes

--- ZionMarketResearch/Scripts/layout.js
+++ ZionMarketResearch/Scripts/layout.js
@@ -1,22 +1,43 @@
 ﻿$(document).ready(function () {
 
     // counter animation
-    $('.count').each(function () {
-        $(this).prop('Counter', 0).animate({
-            Counter: $(this).text()
-        }, {
-            duration: 8000,
-            easing: 'swing',
-            step: function (now) {
-                $(this).text(Math.ceil(now));
-            },
-            complete: function () {
-                if ($(this).hasClass("plus")) {
-                    $(this).text($(this).text() + "+");
-                }
+    //$('.count').each(function () {
+    //    $(this).prop('Counter', 0).animate({
+    //        Counter: $(this).text()
+    //    }, {
+    //        duration: 8000,
+    //        easing: 'swing',
+    //        step: function (now) {
+    //            $(this).text(Math.ceil(now));
+    //        },
+    //        complete: function () {
+    //            if ($(this).hasClass("plus")) {
+    //                $(this).text($(this).text() + "+");
+    //            }
+    //        }
+    //    });
+    //});
+
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+
+                $(entry.target).prop('Counter', 0).animate({
+                    Counter: $(entry.target).text()
+                }, {
+                    duration: 2000,
+                    easing: 'swing',
+                    step: function (now) {
+                        $(entry.target).text(Math.ceil(now));
+                    }
+                });
+
+                observer.unobserve(entry.target);
             }
         });
     });
+
+    document.querySelectorAll('.count').forEach(el => observer.observe(el));
 
     // pricing responsive fix
     if ($(window).width() < 514) {
